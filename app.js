753b6b48367c79e8da886ac34b61e5f7a39faf58
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2022-12-02 19:43:24 +0000

    small project fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const sum = (a,b) => {
     return a + b
 }
-console.log(sum(7,3));
 
 const fromEuroToDollar = function(valueInEuro){
     // convert the given valueInEuro to dollars
@@ -9,18 +8,15 @@ const fromEuroToDollar = function(valueInEuro){
     //return the dollar value
     return valueInDollar;
 }
-console.log(fromEuroToDollar(10));
 
 const fromDollarToYen = (valueInDollar) => {
     let valueInYen = valueInDollar * 106.5;
     return valueInYen
 }
-console.log(fromDollarToYen(10));
 
 const fromYenToPound = (valueInYen) => {
     let valueInPound = valueInYen * 0.006;
     return valueInPound;
 }
-console.log(fromYenToPound(10));
 
 module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
